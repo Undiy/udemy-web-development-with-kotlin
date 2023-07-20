0934167c9567050132be7e5c007d11c199e4522a
Undiy <undiy@yandex.ru>
2023-07-20 14:21:59 +0500

    Added server functionality

--- CatsHostel/src/main/kotlin/Main.kt
+++ CatsHostel/src/main/kotlin/Main.kt
@@ -1,3 +1,32 @@
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.jackson.jackson
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
 fun main() {
-    println("Hello Kotlin Web")
+    val port = 8080
+
+    val server = embeddedServer(Netty, port) {
+        install(ContentNegotiation) {
+            jackson {
+                enable(SerializationFeature.INDENT_OUTPUT)
+            }
+        }
+        routing {
+            get {
+                context.respond(mapOf("Welcome" to "our Cat Hostel"))
+            }
+            get("/{name}") {
+                val name = context.parameters["name"]
+                context.respond(mapOf("Cat name" to name))
+            }
+        }
+    }
+
+    server.start()
 }
